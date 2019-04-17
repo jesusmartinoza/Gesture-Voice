4b059955be20aca33583bf4c33c80c27b2480bc3
Jesús Martínez <hongo-loco@hotmail.com>
2019-04-16 19:09:21 -0500

    Webcam read

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,46 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <h1>Welcome to Your Vue.js App</h1>
+    <div class="container">
+      <h1>Gesture Voice</h1>
+
+      <WebCam />
+    </div>
   </div>
 </template>
+
+<script>
+import WebCam from './components/WebCam.vue'
+
+export default {
+  name: "App",
+  components: {
+    WebCam
+  },
+  created: function() {
+  }
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Montserrat:400,700');
+
+html, .body {
+  background-color: #DADFF2;
+  font-family: 'Montserrat', sans-serif;
+}
+
+h1 {
+  font-weight: bold;
+}
+
+.container {
+  background-color: white;
+  border-radius: 1.5em;
+  box-shadow: 0px 0px 16px #BEC1D6;
+  height: 90vh;
+  margin: 1em auto;
+  padding: 1em;
+  text-align: center;
+  width: 85%;
+}
+</style>
---
+++ src/components/WebCam.vue
@@ -0,0 +1,24 @@
+<template>
+  <video id="video" ref="video" autoplay>
+  </video>
+</template>
+
+<script>
+export default {
+  name: "WebCam",
+  components: {
+  },
+  created: function() {
+    // Start Web Cam
+    navigator.mediaDevices.getUserMedia({video: {facingMode: 'user'}, audio: false})
+      .then((stream) => {
+      this.$refs.video.srcObject = stream;
+      this.$refs.video.width = 227;
+      this.$refs.video.height = 227;
+
+      this.$refs.video.addEventListener('playing', ()=> this.videoPlaying = true);
+      this.$refs.video.addEventListener('paused', ()=> this.videoPlaying = false);
+    })
+  }
+};
+</script>
